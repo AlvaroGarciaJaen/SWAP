fc2374c30e95e2e71a9322070866bf5f94fd0c59
Álvaro García Jaén <garciajaenalvaro@gmail.com>
2019-03-29 21:24:09 +0100

    Actualizado script para todas las máquinas usadas en la práctica 3

--- startVMs.sh
+++ startVMs.sh
@@ -2,3 +2,6 @@
 
 vboxmanage startvm SWAP01-ubu16 --type headless
 vboxmanage startvm SWAP02-ubu16 --type headless
+vboxmanage startvm SWAP03-ubu16 --type headless
+vboxmanage startvm SWAP04-ubu16 --type headless
+vboxmanage startvm SWAP05-ubu16 --type headless
