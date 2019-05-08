e8d35ac1289a814f9aee0b43b7f672f786d26c26
Álvaro García Jaén <garciajaenalvaro@gmail.com>
2019-05-08 09:36:51 +0200

    Cambiado script para iniciar las maquinas interesantes para la nueva practica

--- startVMs.sh
+++ startVMs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 vboxmanage startvm SWAP01-ubu16 --type headless
-vboxmanage startvm SWAP02-ubu16 --type headless
+vboxmanage startvm SWAP02x-ubu16 --type headless
 vboxmanage startvm SWAP03-ubu16 --type headless
-vboxmanage startvm SWAP04-ubu16 --type headless
-vboxmanage startvm SWAP05-ubu16 --type headless
+#vboxmanage startvm SWAP04-ubu16 --type headless
+#vboxmanage startvm SWAP05-ubu16 --type headless
